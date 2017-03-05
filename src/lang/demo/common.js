773e6479e3b9b960ce8a0f139282651d8f055382
SUZUKI Masaya <suzukimasaya428@gmail.com>
2017-03-05 14:51:46 +0900

    JSの共通する関数を集約

--- src/lang/demo/common.js
+++ src/lang/demo/common.js
@@ -1,12 +1,6 @@
-var display_id = "info";
-
 // なでしこの関数をカスタマイズ
 navigator.nako3.getFunc("言").fn = function (msg) {
     alert(msg);
-};
-
-navigator.nako3.getFunc("表示").fn = function (s) {
-    $(display_id).innerHTML += to_html(s) + "<br>";
 };
 
 // なでしこにオリジナル関数をJSで追加
@@ -24,21 +18,6 @@ function $(id) {
 function to_html(s) {
     s = "" + s;
     return s.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;');
-}
-
-function run_box(id) {
-    if (id == null) {
-        alert('idが設定されていません。');
-        return;
-    }
-    var src = $(id).value;
-    display_id = id + "_info";
-    $(display_id).innerHTML = "";
-    try {
-        navigator.nako3.run(src);
-    } catch (e) {
-        console.log(e);
-    }
 }
 
 function reset_box() {
--- src/lang/demo/index.js
+++ src/lang/demo/index.js
@@ -1,29 +1,9 @@
 // なでしこの関数をカスタマイズ
-navigator.nako3.getFunc("言").fn = function (msg) {
-    alert(msg);
-};
-
 navigator.nako3.getFunc("表示").fn = function (s) {
     $("info").innerHTML += to_html(s) + "<br>";
 };
 
-// なでしこにオリジナル関数をJSで追加
-navigator.nako3.addFunc("色変更",
-    [["に", "へ"]],
-    function (s) {
-        $("info").style.color = s;
-    });
-
 // 簡易DOMアクセス関数など
-function $(id) {
-    return document.getElementById(id);
-}
-
-function to_html(s) {
-    s = "" + s;
-    return s.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;');
-}
-
 function run_box(id) {
     if (id != null) {
         $('src_box').value = $(id).value;
@@ -42,8 +22,4 @@ function run_box(id) {
         $("err").innerHTML = msg;
         console.log(e);
     }
-}
-
-function reset_box() {
-    $("info").innerHTML = "";
 }
