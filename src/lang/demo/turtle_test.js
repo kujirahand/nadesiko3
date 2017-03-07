f24cec134c1df0a8c5ecc70ccf26aae85ea30e03
kujirahand <web@kujirahand.com>
2017-03-07 15:40:08 +0900

    タートルグラフィックスのデモで、タートルプラグインを読み込むようにした

--- src/lang/demo/turtle_test.js
+++ src/lang/demo/turtle_test.js
@@ -3,3 +3,8 @@ navigator.nako3.setFunc("表示", function (s) {
     $("info").innerHTML += to_html(s) + "<br>";
 });
 
+// なでしこシステムにタートルグラフィックスを追加
+navigator.nako3.addPluginObject("PluginTurtle", PluginTurtle);
+
+
+
