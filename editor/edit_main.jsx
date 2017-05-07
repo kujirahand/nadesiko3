be4e68e5fae5655e5d1df32dc80cc505f1cda0a2
SUZUKI Masaya <suzukimasaya428@gmail.com>
2017-05-08 05:23:58 +0900

    jQuery導入

--- editor/edit_main.jsx
+++ editor/edit_main.jsx
@@ -1,7 +1,8 @@
 // edit_main.js
+import $ from 'jquery'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import CommandListComponent from './command_list_component'
 
 // render
-ReactDOM.render(<CommandListComponent />, document.getElementById('command-list-component'))
+ReactDOM.render(<CommandListComponent />, $('#command-list-component')[0])
