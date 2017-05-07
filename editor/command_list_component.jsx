d3bd89a866386222feca850c423d8e0a6555f256
SUZUKI Masaya <suzukimasaya428@gmail.com>
2017-05-07 14:37:17 +0900

    命令一覧のコンポーネントを再設計

---
+++ editor/command_list_component.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import CommandListButton from './command_list_button'
+import CommandList from './command_list'
+
+export default class CommandListComponent extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {flagShow: false}
+  }
+
+  render () {
+    return (
+      <div>
+        <CommandListButton flagShow={this.state.flagShow}
+                           onChanged={(e) => this.setState({'flagShow': !this.state.flagShow})} />
+        <CommandList flagShow={this.state.flagShow} />
+      </div>
+    )
+  }
+}
