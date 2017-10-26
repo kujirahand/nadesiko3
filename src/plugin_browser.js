0267bfd4e81222f70ccd2dd4364a93d4f430927b
kujirahand <web@kujirahand.com>
2017-10-26 23:52:57 +0900

    plugin_browserで「終わる」の定義が間違っていたので修正

--- src/plugin_browser.js
+++ src/plugin_browser.js
@@ -33,7 +33,7 @@ const PluginBrowser = {
   '終': { // @ブラウザでプログラムの実行を強制終了する // @おわる
     type: 'func',
     josi: [],
-    fn: function (s) {
+    fn: function () {
       throw new Error('__終わる__')
     },
     return_none: true
