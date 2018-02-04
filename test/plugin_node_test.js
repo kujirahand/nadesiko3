4ec61477f27d64b093961a6cba04ab20ada0f94f
kujirahand <web@kujirahand.com>
2018-02-04 19:38:46 +0900

    テストを消してしまったので追加

---
+++ test/plugin_node_test.js
@@ -0,0 +1,38 @@
+const assert = require('assert')
+const path = require('path')
+const NakoCompiler = require('../src/nako3')
+const PluginNode = require('../src/plugin_node')
+
+describe('node_func', () => {
+  const nako = new NakoCompiler()
+  nako.addPluginFile('PluginNode', 'plugin_node.js', PluginNode)
+  // nako.debug = true
+  const cmp = (code, res) => {
+    if (nako.debug) {
+      console.log('code=' + code)
+    }
+    assert.equal(nako.runReset(code).log, res)
+  }
+  const cmd = (code) => {
+    if (nako.debug) console.log('code=' + code)
+    nako.runReset(code)
+  }
+  // --- test ---
+  it('表示', () => {
+    cmp('3を表示', '3')
+    cmp('1+2*3を表示', '7')
+    cmp('A=30;「--{A}--」を表示', '--30--')
+  })
+  it('存在', () => {
+    cmp('「/xxx/xxx/xxx/xxx」が存在;もしそうならば;「NG」と表示。違えば「OK」と表示。', 'OK')
+    const fname = path.join(__dirname, 'node_func.js')
+    cmp('「' + fname + '」が存在;もしそうならば;「OK」と表示。違えば「NG」と表示。', 'OK')
+  })
+  it('ASSERT', () => {
+    cmd('3と3がASSERT等')
+  })
+  it('環境変数取得', () => {
+    const path = process.env['PATH']
+    cmp('「PATH」の環境変数取得して表示。', path)
+  })
+})
---
+++ test/plugin_test.js
@@ -0,0 +1,20 @@
+const assert = require('assert')
+const NakoCompiler = require('../src/nako3')
+const PluginNode = require('../src/plugin_node')
+
+describe('node_plugins_test', () => {
+  const nako = new NakoCompiler()
+  nako.addPluginFile('PluginNode', 'PluginNode', PluginNode)
+  // nako.debug = true
+  const cmp = (code, res) => {
+    if (nako.debug) {
+      console.log('code=' + code)
+    }
+    const ret = nako.runReset(code)
+    assert.equal(ret.log, res)
+  }
+  // TODO: うまく動かない (ただし単体でテストすると動くので、原因を追及する)
+  it('「取り込む」', () => {
+     cmp('!「nadesiko3-hoge」を取り込む。3と5をHOGE足して、表示。', '8')
+  })
+})
