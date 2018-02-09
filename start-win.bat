3e257b91139d251232984dddae6a11e502d2cebe
kujirahand <web@kujirahand.com>
2018-02-09 10:55:15 +0900

    NPMを使ったインストールではインストール時に開発環境のインストールが必要なので、一度、インストーラーを破棄(#178)

---
+++ start-win.bat
@@ -0,0 +1,39 @@
+@echo off
+
+rem --- set env for nadesiko3 ---
+SET NAKO_HOME=%~dp0
+SET PATH=%NAKO_HOME%;%NAKO_HOME%bin;%PATH%
+
+rem --- cd here ---
+cd %NAKO_HOME%
+
+rem --- check param ---
+SET NAKO_MODE="server"
+
+IF "%1" == "/cmd" (
+  SET NAKO_MODE="cmd"
+  ECHO start command mode
+)
+IF "%1" == "/test" (
+  SET NAKO_MODE="cmd"
+  ECHO start test
+  call .\nodejs\npm test
+)
+IF "%1" == "/install" (
+  SET NAKO_MODE="cmd"
+  ECHO install files for windows
+  call .\nodejs\node .\installer\setup-win.bat
+)IF %NAKO_MODE%=="server" (
+  rem --- exec server ---
+  call .\nodejs\npm run server
+)
+
+rem --- check error ---
+IF "%ERRORLEVEL%"=="9009" (
+  ECHO ------------------------------------
+  ECHO [ERROR] Nadesiko3 packaging error
+  ECHO ------------------------------------
+  PAUSE
+  EXIT
+)
+
