8f8f20dee9609e65f74d26b5cde41b3da2850839
kujirahand <web@kujirahand.com>
2018-03-29 01:16:21 +0800

    バージョンを 3.0.38 に変更

--- src/plugin_system.js
+++ src/plugin_system.js
@@ -3,7 +3,7 @@ const PluginSystem = {
     type: 'func',
     josi: [],
     fn: function (sys) {
-      sys.__varslist[0]['ナデシコバージョン'] = '3.0.37'
+      sys.__varslist[0]['ナデシコバージョン'] = '3.0.38'
       // システム関数を探す
       sys.__getSysValue = function (name, def) {
         if (sys.__v0[name] === undefined) return def
