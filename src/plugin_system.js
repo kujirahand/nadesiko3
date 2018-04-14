2185b28267849a6c0db8cd272ff818b49ac541cd
kujirahand <web@kujirahand.com>
2018-04-14 22:19:43 +0800

    v3.0.41

--- src/plugin_system.js
+++ src/plugin_system.js
@@ -3,7 +3,7 @@ const PluginSystem = {
     type: 'func',
     josi: [],
     fn: function (sys) {
-      sys.__varslist[0]['ナデシコバージョン'] = '3.0.40'
+      sys.__v0['ナデシコバージョン'] = '3.0.41'
       // システム関数を探す
       sys.__getSysValue = function (name, def) {
         if (sys.__v0[name] === undefined) return def
