466e4a7d97c2a80e56493b0b96bf12553d9803bd
massongit <suzukimasaya428@gmail.com>
2019-01-03 10:49:58 +0900

    CSV取得, TSV取得を修正

--- src/plugin_system.js
+++ src/plugin_system.js
@@ -1523,7 +1523,17 @@ const PluginSystem = {
       for (let i = 0; i < split_quote.length; i++) {
         const s_q = split_quote[i]
 
-        if (i % 2 === 0) { //偶数番目の要素 (さらに区切れる可能性がある) のとき
+        if (s_q === '') { // エスケープされたダブルクオーテーションのとき
+          // ダブルクオーテーション
+          const quote = '"'
+
+          // 行の末尾の要素にダブルクオーテーションを追加
+          if (data_row.length === 0) data_row.push(quote)
+          else data_row[data_row.length - 1] += quote
+
+          // ダブルクオーテーションフラグを立てる
+          is_quote = true
+        } else if (i % 2 === 0) { //偶数番目の要素 (さらに区切れる可能性がある) のとき
           let s
 
           if (is_quote) { // ダブルクオーテーションが立っているとき、行の末尾の要素を取り出す (今見ている要素と連結させるため)
@@ -1549,17 +1559,10 @@ const PluginSystem = {
               }
             }
           }
-        } else if (s_q === '') { // 奇数番目の要素で空文字列 (エスケープされたダブルクオーテーション) のとき
-          // ダブルクオーテーション
-          const quote = '"'
-
-          // 行の末尾の要素にダブルクオーテーションを追加
-          if (data_row.length === 0) data_row.push(quote)
-          else data_row[data_row.length - 1] += quote
-
-          // ダブルクオーテーションフラグを立てる
-          is_quote = true
-        } else data_row.push(s_q) // 奇数番目の要素 (これ以上区切れない) で、空文字列でないとき、文字列を行に追加
+        } else if (is_quote) { // 奇数番目の要素 (これ以上区切れない) で、ダブルクオーテーションが立っているとき
+          data_row[data_row.length - 1] += s_q
+          is_quote = false
+        } else data_row.push(s_q) // 奇数番目の要素 (これ以上区切れない) で、ダブルクオーテーションが立っていないとき
       }
 
       return data
