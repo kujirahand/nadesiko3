bb33cfc56fe0cbef1d97f6594d6dc163709bc2ef
massongit <suzukimasaya428@gmail.com>
2019-12-24 21:24:35 +0900

    importの記述方法を変更

--- src/plugin_browser.js
+++ src/plugin_browser.js
@@ -2,7 +2,7 @@
 require('es6-promise').polyfill()
 require('isomorphic-fetch')
 
-import hotkeys from 'hotkeys-js'
+const hotkeys = require('hotkeys-js')
 
 const errMsgCanvasInit = '描画を行うためには、HTML内にcanvasを配置し、idを振って『描画開始』命令に指定します。'
 
