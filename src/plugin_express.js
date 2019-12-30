5c7b9db5f9e18c8d8c0e1dedf610fc3cd6fd3214
kujirahand <web@kujirahand.com>
2019-12-30 21:22:18 +0800

    『WEBサーバ起動』が動かなくなっていたので修正。

--- src/plugin_express.js
+++ src/plugin_express.js
@@ -5,12 +5,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
-let server = null
-let app = null
-let debug = true
 // 定数・変数
 let WEBSERVER_NAME = 'WEBサーバ(なでしこ+Express)'
 const ERROR_NO_INIT = '先に『WEBサーバ起動』命令を実行してください。'
+let debug = true
 
 const PluginExpress = {
   '初期化': {
@@ -22,6 +20,8 @@ const PluginExpress = {
       sys.__v0['WEBサーバ:要求'] = null
       sys.__v0['WEBサーバ:応答'] = null
       sys.__v0['WEBサーバクエリ'] = {}
+      sys.__server = null
+      sys.__webapp = null
     }
   },
   // @Webサーバ(Express)
@@ -38,13 +38,13 @@ const PluginExpress = {
   },
   'WEBサーバ起動': { // @ポートPORTNOでWebサーバを起動して成功したら『WEBサーバ起動成功した時』を実行する // @WEBさーばきどう
     type: 'func',
-    josi: [['まで'], ['の', 'で']],
-    fn: function (callback, portno, sys) {
-      app = express()
-      server = app.listen(portno, () => {
+    josi: [['の', 'で']],
+    fn: function (portno, sys) {
+      let app = express()
+      let server = app.listen(portno, () => {
         const pno = server.address().port
         if (debug) {
-          console.log('* [' + WEBSERVER_NAME + ']')
+          console.log('* [' + WEBSERVER_NAME + '] (debug)')
           console.log('| 以下のURLで起動しました。')
           console.log('+- [URL] http://localhost:' + pno)
         }
@@ -66,7 +66,18 @@ const PluginExpress = {
         type: 'application/x-www-form-urlencoded',
         extended: true
       }))
+      // memo
+      sys.__webapp = app
+      sys.__server = server
       return server
+    }
+  },
+  'WEBサーバ起動時': { // @ポートPORTNOでWebサーバを起動して成功したらCALLBACKを実行する // @WEBさーばきどうしたとき
+    type: 'func',
+    josi: [['を'],['の', 'で']],
+    fn: function (callback, portno, sys) {
+      sys.__v0['WEBサーバ:ONSUCCESS'] = callback
+      return sys.__exec('WEBサーバ起動', [portno, sys])
     }
   },
   'WEBサーバ起動成功時': { // @WEBサーバ起動が成功した時にcallbackを実行 // @WEBさーばきどうせいこうしたとき
@@ -88,10 +99,10 @@ const PluginExpress = {
   'WEBサーバ静的パス指定': { // @サーバのHTMLや画像などを配置する静的パスを指定する // @WEBさーばせいてきぱすしてい
     type: 'func',
     josi: [['を'], ['に', 'へ']],
-    fn: function (url, path) {
-      if (app == null) {throw new Error(ERROR_NO_INIT)}
-      if (debug) {console.log('static', url, path)}
-      app.use(url, express.static(path))
+    fn: function (url, path, sys) {
+      if (sys.__webapp == null) {throw new Error(ERROR_NO_INIT)}
+      if (debug) {console.log('[static]', url, path)}
+      sys.__webapp.use(url, express.static(path))
     },
     return_none: true
   },
@@ -99,7 +110,12 @@ const PluginExpress = {
     type: 'func',
     josi: [['を'], ['に', 'へ']],
     fn: function (callback, uri, sys) {
-      app.get(uri, (req, res) => { callbackServerFunc(callback, req, res, sys) })
+      if (debug) {
+        console.log('[GET] ' + uri)
+      }
+      sys.__webapp.get(uri, (req, res) => { 
+        callbackServerFunc(callback, req, res, sys) 
+      })
     },
     return_none: true
   },
@@ -107,7 +123,7 @@ const PluginExpress = {
     type: 'func',
     josi: [['を'], ['に', 'へ']],
     fn: function (callback, uri, sys) {
-      app.post(uri, (req, res) => { callbackServerFunc(callback, req, res, sys) })
+      sys.__webapp.post(uri, (req, res) => { callbackServerFunc(callback, req, res, sys) })
     },
     return_none: true
   },
@@ -115,7 +131,7 @@ const PluginExpress = {
     type: 'func',
     josi: [['を'], ['に', 'へ']],
     fn: function (callback, uri, sys) {
-      app.put(uri, (req, res) => { callbackServerFunc(callback, req, res, sys) })
+      sys.__webapp.put(uri, (req, res) => { callbackServerFunc(callback, req, res, sys) })
     },
     return_none: true
   },
@@ -123,7 +139,7 @@ const PluginExpress = {
     type: 'func',
     josi: [['を'], ['に', 'へ']],
     fn: function (callback, uri, sys) {
-      app.delete(uri, (req, res) => { callbackServerFunc(callback, req, res, sys) })
+      sys.__webapp.delete(uri, (req, res) => { callbackServerFunc(callback, req, res, sys) })
     },
     return_none: true
   },
@@ -149,6 +165,7 @@ const PluginExpress = {
 
 // GET/POST/PUT/DELETEのコールバック
 function callbackServerFunc (callback, req, res, sys) {
+  // if (debug) { console.log(req) }
   sys.__v0['WEBサーバ:要求'] = req
   sys.__v0['WEBサーバ:応答'] = res
   sys.__v0['GETデータ'] = req.query
