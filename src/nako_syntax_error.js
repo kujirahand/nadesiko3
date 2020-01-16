22d9661e5b4f86ddbf19dac8a305d3a8e7ea943e
kujirahand <web@kujirahand.com>
2020-01-16 14:17:57 +0800

    Windows用のインストーラーのテンプレートを修正 - nakopadの実行で作成するバッチファイルを調整。

--- src/nako_syntax_error.js
+++ src/nako_syntax_error.js
@@ -7,11 +7,13 @@ class NakoSyntaxError extends Error {
   constructor (msg, line, fname) {
     const line2 = line + 1
     const fname2 = (fname === undefined) ? '' : fname
+    // Get Nadesiko Version
     const dummy = {__v0: {}}
     PluginSystem['!PluginSystem:初期化'].fn(dummy)
-    const title = `[文法エラー]${fname}(${line2}行目): ${msg}\n[version]: ${dummy.__v0.ナデシコバージョン}`
+    // Error Message Format
+    const title = `[文法エラー]${fname2}(${line2}行目): ${msg}\n` +
+                  `[バージョン] ${dummy.__v0.ナデシコバージョン}`
     super(title)
-    
   }
 }
 
