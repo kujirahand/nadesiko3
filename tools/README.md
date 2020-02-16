5530ed3720163620eaed03186ad0d50399edd665
massongit <suzukimasaya428@gmail.com>
2020-02-16 12:31:57 +0900

    READMEに対応機器, 対応ブラウザを明示的に埋め込めるよう実装

--- tools/README.md
+++ tools/README.md
@@ -6,5 +6,6 @@
 
 なでしこ3(Node版)のエディタ
 
-
-
+## readme_generator
+[README.template.md](../README.template.md)を元に対応機器, 対応ブラウザを展開した[README.md]](../README.md)を生成します。
+なお、[README.template.md](../README.template.md)を記述する際は対応機器, 対応ブラウザを展開したい場所に `{embed browser}` と記述する必要があります。
---
+++ tools/readme_generator/generator.js
@@ -0,0 +1,83 @@
+const fs = require('fs')
+const browserslist = require('browserslist')
+const agents = require('caniuse-db/data.json').agents
+
+const browsers = {}
+
+for (const browserStr of browserslist()) {
+  const browser = browserStr.split(' ')
+  let type = 'その他'
+  let device = ''
+
+  if (browser[0] in agents) {
+    const browserData = agents[browser[0]]
+
+    browser[0] = browserData.browser
+
+    switch (browserData.type) {
+      case 'mobile':
+        type = 'Webブラウザ'
+        device = 'モバイル'
+        break
+      case 'desktop':
+        type = 'Webブラウザ'
+        device = 'PC'
+        break
+      default:
+        type = browserData.type
+        break
+    }
+  } else if (browser[0] === 'node') {
+    type = 'PC (Windows/macOS/Linux)'
+    browser[0] = 'Node'
+  }
+
+  if (!(type in browsers)) {
+    browsers[type] = {}
+  }
+
+  if (!(device in browsers[type])) {
+    browsers[type][device] = {}
+  }
+
+  if (!(browser[0] in browsers[type][device])) {
+    browsers[type][device][browser[0]] = []
+  }
+
+  browsers[type][device][browser[0]].push(browser[1])
+}
+
+const browserMd = []
+
+for (const type in browsers) {
+  browserMd.push('* ' + type)
+
+  for (const device in browsers[type]) {
+    let indent = ''
+
+    if (device !== '') {
+      browserMd.push('\t* ' + device)
+      indent = '\t'
+    }
+
+    for (const browser in browsers[type][device]) {
+      browserMd.push(indent + '\t* ' + browser)
+
+      for (const version of browsers[type][device][browser]) {
+        browserMd.push(indent + '\t\t* ' + version)
+      }
+    }
+  }
+}
+
+let md = []
+
+for (const row of fs.readFileSync('README.template.md').toString().split('\n')) {
+  if (row === '{embed browser}') {
+    md = md.concat(browserMd)
+  } else {
+    md.push(row)
+  }
+}
+
+fs.writeFileSync('README.md', md.join('\n'))
