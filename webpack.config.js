1e0ae849fc9852cf903afef288d020fd7c5b8645
massongit <suzukimasaya428@gmail.com>
2020-03-22 17:12:39 +0900

    コメント追加

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
   // devtool: 'cheap-module-eval-source-map',
 
   plugins: [
+    // TODO: 日付計算ライブラリを置き換えたら削除
     // moment.jsのロケールファイルをビルド対象から除外
     // ロケールファイルを使用したい場合は、
     // ソースコード内で『require('moment/locale/hoge')』のように記述すれば、そのロケールファイルのみビルド対象に含まれる
