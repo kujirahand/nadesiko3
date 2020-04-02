1e60337210261fa5e3a5fb948a1568c7cee41dd2
kujirahand <kujira@kujirahand.com>
2020-04-02 18:10:53 +0900

    微修正

--- src/plugin_system.js
+++ src/plugin_system.js
@@ -5,7 +5,7 @@ const PluginSystem = {
     type: 'func',
     josi: [],
     fn: function (sys) {
-      sys.__v0['ナデシコバージョン'] = '3.0.70'
+      sys.__v0['ナデシコバージョン'] = '3.0.71'
       // システム関数を探す
       sys.__getSysValue = function (name, def) {
         if (sys.__v0[name] === undefined) {return def}
