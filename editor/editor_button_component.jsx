4657b2aea57335c33e001b8a1d23f2cf4850855b
massongit <suzukimasaya428@gmail.com>
2020-07-18 23:15:20 +0900

    処理を分離

--- editor/editor_button_component.jsx
+++ editor/editor_button_component.jsx
@@ -3,6 +3,11 @@ import PropTypes from 'prop-types'
 import dayjs from 'dayjs'
 
 export default class EditorButtonComponent extends React.Component {
+  constructor (props) {
+    super(props)
+    this.preCode = this.props.preCode + '\n'
+  }
+
   getUsedFuncs (
     ast,
     funcs = {
@@ -27,50 +32,55 @@ export default class EditorButtonComponent extends React.Component {
     return funcs
   }
 
+  onRunButtonClick () {
+    try {
+      // なでしこの関数をカスタマイズ --- TODO: なでしこの追加命令は edit_main.jsxで書いているので別途関数を作ってそこでまとめるように
+      this.props.nako3.setFunc('表示', [['と', 'を', 'の']], this.props.onInformationChanged)
+      window.localStorage['nako3/editor/code'] = this.props.code
+      this.props.nako3.run(this.preCode + this.props.code)
+      const funcs = this.getUsedFuncs(this.props.nako3.ast)
+
+      for (const func of funcs.def) {
+        funcs.used.delete(func)
+      }
+
+      this.props.onUsedFuncsChanged(funcs.used)
+    } catch (e) {
+      this.props.onErrorChanged(e)
+    }
+  }
+
+  onTestButtonClick () {
+    try {
+      window.localStorage['nako3/editor/code'] = this.props.code
+      this.props.nako3.test(this.preCode + this.props.code)
+    } catch (e) {
+      this.props.onErrorChanged(e)
+    }
+  }
+
+  onDownloadButtonClick () {
+    try {
+      const link = document.createElement('a')
+      link.href = window.URL.createObjectURL(new Blob([this.props.nako3.compile(this.preCode + this.props.code)]))
+      link.download = 'nako3_' + dayjs().format('YYYYMMDDHHmmss') + '.js'
+      link.click()
+    } catch (e) {
+      this.props.onErrorChanged(e)
+    }
+  }
+
   render () {
-    const preCode = this.props.preCode + '\n'
     return (
       <div className="buttons">
-        <button onClick={() => {
-          try {
-            // なでしこの関数をカスタマイズ --- TODO: なでしこの追加命令は edit_main.jsxで書いているので別途関数を作ってそこでまとめるように
-            this.props.nako3.setFunc('表示', [['と', 'を', 'の']], this.props.onInformationChanged)
-            window.localStorage['nako3/editor/code'] = this.props.code
-            this.props.nako3.run(preCode + this.props.code)
-            const funcs = this.getUsedFuncs(this.props.nako3.ast)
-
-            for (const func of funcs.def) {
-              funcs.used.delete(func)
-            }
-
-            this.props.onUsedFuncsChanged(funcs.used)
-          } catch (e) {
-            this.props.onErrorChanged(e)
-          }
-        }} className="default_button">
+        <button onClick={this.onRunButtonClick.bind(this)} className="default_button">
           実行
         </button>
-        <button onClick={() => {
-          try {
-            window.localStorage['nako3/editor/code'] = this.props.code
-            this.props.nako3.test(preCode + this.props.code)
-          } catch (e) {
-            this.props.onErrorChanged(e)
-          }
-        }} className="default_button">
+        <button onClick={this.onTestButtonClick.bind(this)} className="default_button">
           テスト
         </button>
         <button onClick={this.props.onReset} className="default_button">クリア</button>
-        <button onClick={() => {
-          try {
-            const link = document.createElement('a')
-            link.href = window.URL.createObjectURL(new Blob([this.props.nako3.compile(preCode + this.props.code)]))
-            link.download = 'nako3_' + dayjs().format('YYYYMMDDHHmmss') + '.js'
-            link.click()
-          } catch (e) {
-            this.props.onErrorChanged(e)
-          }
-        }}>
+        <button onClick={this.onDownloadButtonClick.bind(this)}>
           ↓
         </button>
       </div>
