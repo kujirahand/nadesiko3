3bec2a54a26268e57f29d9105da91d6f0189ac63
kujirahand <kujira@kujirahand.com>
2020-08-16 10:53:45 +0900

    requireで拡張子を明示 #562

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 module.exports = {
-  // Node.jsでモジュールモードでなでしこを利用する場合
+  // Node.jsのモジュールモードでなでしこを利用する場合
   'compiler': require('./nako3.js'),
   'PluginNode': require('./plugin_node.js'),
 
-  // Electron
-  'app': require('./enako3').app
+  // Electronでなでしこを利用する場合
+  'app': require('./enako3.js').app
 }
 
