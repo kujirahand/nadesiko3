9195e5622911d136cbbb59b4f06dc86210256e9d
kujirahand <kujira@kujirahand.com>
2020-08-16 11:21:13 +0900

    editor.jsからなでしこ(wnako3.js)を分離。 #565

--- editor/edit_main.jsx
+++ editor/edit_main.jsx
@@ -2,7 +2,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import EditorComponent from './editor_component'
-import WebNakoCompiler from '../src/wnako3'
 import 'mocha-css'
 
 // なでしこコンパイラ唯一のインスタンス
