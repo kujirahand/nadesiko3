72a02080710650d7ef993278d8e13158eae02e82
kujirahand <kujira@kujirahand.com>
2020-10-01 22:32:56 +0900

    v1互換の「テスト実行」命令を「なる」命令に代わって追加。

--- src/plugin_test.js
+++ src/plugin_test.js
@@ -12,15 +12,23 @@ const PluginTest = {
       assert.strictEqual(a, b)
     }
   },
-  // @テスト
-  'なる': { // @ mochaによるテストで、ASSERTでAとBが正しいことを報告する // @なる
+  'テスト実行': { // @ mochaによるテストで、ASSERTでAとBでテスト実行してAとBが等しいことを報告する // @てすとじっこう
     type: 'func',
-    josi: [['と'], ['に']],
+    josi: [['と'], ['で']],
     fn: function (a, b, sys) {
       const assert = require('assert')
       assert.strictEqual(a, b)
     }
-  }
+  },
+  'テスト等しい': { // @ mochaによるテストで、ASSERTでAとBが正しいことを報告する // @テストひとしい
+    type: 'func',
+    josi: [['と'], ['が']],
+    fn: function (a, b, sys) {
+      const assert = require('assert')
+      assert.strictEqual(a, b)
+    }
+  },
+  
 }
 
 module.exports = PluginTest
