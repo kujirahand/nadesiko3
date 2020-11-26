0d898359fff3a0afb9c36a9af0f58e8faff1708b
weyk <weyk@nifty.com>
2020-11-26 23:44:36 +0900

    取り込む命令の構造を変更してcnako/wnako対応 parseを同基/非同期の二種類に変更。あわせて、compile/run/testも連鎖して二種に。 kansujiにブラウザ環境での自動addPlugin追加

--- src/wnako3.js
+++ src/wnako3.js
@@ -1,6 +1,9 @@
 // nadesiko for web browser
 // wnako3.js
+require('node-fetch')
+
 const NakoCompiler = require('./nako3')
+const NakoRequiePlugin = require('./nako_require_plugin_helper')
 const PluginBrowser = require('./plugin_browser')
 const NAKO_SCRIPT_RE = /^(なでしこ|nako|nadesiko)3?$/
 
@@ -8,6 +11,8 @@ class WebNakoCompiler extends NakoCompiler {
   constructor () {
     super()
     this.__varslist[0]['ナデシコ種類'] = 'wnako3'
+    this.beforeParseCallback = this.beforeParse
+    this.requirePlugin = new NakoRequiePlugin(this)
   }
 
   /**
@@ -70,6 +75,60 @@ class WebNakoCompiler extends NakoCompiler {
     }
 
     return code
+  }
+
+  // トークンリストからプラグインのインポートを抜き出して処理する
+  beforeParse (opts) {
+    const tokens = opts.tokens
+    const filelist = this.requirePlugin.checkAndPickupRequirePlugin(tokens)
+    if (filelist.length > 0) {
+      return new Promise((allresolve, allreject) => {
+        const nako3 = opts.nako3
+        const pluginpromise = []
+        filelist.forEach(filename => {
+          pluginpromise.push(new Promise((resolve, reject) => {
+            fetch(filename, { mode: 'no-cors' })
+            .then(res => {
+              if (res.ok) {
+                return res.text()
+              }
+              reject(new Error('fail load plugin'))
+            }).then(txt => {
+              resolve(txt)
+            }).catch(err => {
+              reject(err)
+            })
+          }))
+        })
+
+        Promise.all(pluginpromise).then(modules => {
+          modules.forEach(module => {
+            if (/navigator\.nako3\.addPluginObject/.test(module)) {
+              // for auto registration plugin, exetute only
+              window.eval(module)
+            } else
+            if (/module\.exports\s*=/.test(module)) {
+              // for commonjs structure plugin
+              allreject(new Error('no suppout type plugin(commonjs)'))
+            } else {
+              allreject(new Error('no suppout type plugin(unknown)'))
+            }
+            /*
+            // for module structure plugin, regist each expoted key
+            Object.keys(module).forEach((key) => {
+              console.log('[Plugin]' + key)
+              nako3.addPluginObject(key, module[key])
+            })
+            */
+          })
+          allresolve(tokens)
+        }).catch(err => {
+          allreject(err)
+        })
+        //  throw new Error('no support dynamic import at browser envrionment')
+      })
+    }
+    return tokens
   }
 }
 
