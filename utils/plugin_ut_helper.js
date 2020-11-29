264b6b486b4c810c75bc2f8c568595a85d10f640
weyk <weyk@nifty.com>
2020-11-29 23:48:45 +0900

    通常のtestにborwserプラグインのファイル単体テストを追加

---
+++ utils/plugin_ut_helper.js
@@ -0,0 +1,33 @@
+const assert = require('assert').strict
+
+class PluginUtHelper {
+  constructor (targePlugin) {
+    this.plugin = targePlugin
+  }
+
+  cmptypeof (typename, obj) {
+    assert.equal((typeof obj).toLowerCase(), typename.toLowerCase())
+  }
+
+  cmpinstanceof (classtype, obj) {
+    assert.ok(obj instanceof classtype, 'not as specified instance')
+  }
+
+  cmpfn (funcname, args, res) {
+    assert.equal(this.plugin[funcname].fn.apply(this, args), res)
+  }
+
+  cmpifn (funcname, args, res) {
+    assert.equal(this.plugin[funcname].fn.apply(this, args).toUpperCase(), res.toUpperCase())
+  }
+
+  cmpfnex (funcname, args, err, msg) {
+    assert.throws(() => this.plugin[funcname].fn.apply(this, args), { name: err, message: msg })
+  }
+
+  cmpfnexdetail (funcname, args, err) {
+    assert.throws(() => this.plugin[funcname].fn.apply(this, args), err)
+  }
+}
+
+module.exports = { PluginUtHelper }
