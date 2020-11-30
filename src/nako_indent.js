7198630d2b25dd83c2670f37d5aa9d66c93affb3
kujirahand <kujira@kujirahand.com>
2020-11-30 22:56:54 +0900

    インデント構文の試験導入 #596

--- src/nako_indent.js
+++ src/nako_indent.js
@@ -5,26 +5,32 @@
  */
 function convert(code) {
     // プログラム冒頭に「##インデント構文」があれば変換
-    const keywords = ['##インデント構文\n', '＃＃インデント構文\n', '##ここまでだるい\n', '＃＃ここまでだるい\n']
-    const s10 = code.substr(0, 10)
-    if (keywords.indexOf(s10) >= 0) {
-        return convertGo(code.substr(10)+'\n')
+    const keywords = ['##インデント構文', '＃＃インデント構文', '##ここまでだるい', '＃＃ここまでだるい']
+    const s9 = code.substr(0, 9)
+    if (keywords.indexOf(s9) >= 0) {
+        return convertGo(code)
     }
     return code
 }
 
 function convertGo(code) {
+    const END = 'ここまで'
     const lines = code.split('\n')
     const lines2 = []
     const indentStack = []
     let lastIndent = 0
     lines.forEach((line) => {
+        // trim line
+        const lineTrimed = line.replace(/^\s+/, '').replace(/\s+$/, '')
+        if (lineTrimed === '') { return }
+
         // check indent
         const indent = countIndent(line)
         if (lastIndent == indent) {
             lines2.push(line)
             return
         }
+
         // indent
         if (lastIndent < indent) {
             indentStack.push(lastIndent)
@@ -34,23 +40,33 @@ function convertGo(code) {
         }
         // unindent
         if (lastIndent > indent) {
-            if (line != '') {
-                lines2.push(line)
-            }
+            // 5回
+            //   3回
+            //     1を表示
+            //   |
+            // |
             lastIndent = indent
-            while(indentStack.length > 0) {
-                topIndent = indentStack[indentStack.length - 1]
-                if (topIndent <= indent) {
-                    lines2.push(makeIndent(indent) + 'ここまで')
-                    indentStack.pop()
-                    continue
+            while (indentStack.length > 0) {
+                const n = indentStack.pop()
+                if (n == indent) {
+                    if (lineTrimed != '違えば') {
+                        lines2.push(makeIndent(n) + END)
+                    }
+                    lines2.push(line)
+                    return
                 }
-                if (topIndent > indent) {
-                    throw new Exception('インデントが壊れています')
+                if (indent < n) {
+                    lines2.push(makeIndent(n) + END)
+                    continue
                 }
             }
         }
     })
+    // 残りのインデントを処理
+    while (indentStack.length > 0) {
+        const n = indentStack.pop()
+        lines2.push(makeIndent(n) + END)
+    }
     return lines2.join('\n')
 }
 
@@ -62,6 +78,10 @@ function makeIndent(count) {
     return s
 }
 
+/**
+ * インデントの個数を数える
+ * @param {String}} line 
+ */
 function countIndent(line) {
     let cnt = 0
     for (let i = 0; i < line.length; i++) {
