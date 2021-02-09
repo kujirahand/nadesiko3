801d5afdd6d250f813562ac0d03af2cdbbdef42d
kujirahand <kujira@kujirahand.com>
2021-02-09 19:06:36 +0900

    chromebook(linux)向けのインストーラーを追加 #772

---
+++ installer/chromeos.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Node.jsをインストール
+sudo apt update
+sudo apt install -y nodejs npm
+
+# Node.jsを最新版にアップデート
+sudo npm install n -g
+sudo n stable
+sudo ln -sf /usr/local/bin/node /usr/bin/node
+sudo ln -sf /usr/local/bin/npm /usr/bin/npm
+
+# なでしこ3をインストール
+sudo npm install -g nadesiko3
+
+
