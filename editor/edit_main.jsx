cb9c9102570183e6878e7478fc967ef3d03cd58b
yy0931 <54441600+yy0931@users.noreply.github.com>
2021-02-28 22:37:07 +0900

    エディタのIE対応
    JSONをfetchではなくrequireで取得、mochaのscriptタグをhtmlの最後へ移動

--- editor/edit_main.jsx
+++ editor/edit_main.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'mocha-css'
 import dayjs from 'dayjs'
-import CommandList from './command_list'
+/** @type {Record<string, Record<string, string[][]>>} */
+const commandListJSON = require('../release/command.json')
 
 /** @returns {import('../src/wnako3')} */
 const getNako3 = () => navigator.nako3
@@ -17,7 +18,40 @@ const mustGetElementById = (id) => {
   return element
 }
 
-/** @type {React.FC<{ code: string, editorId: number }>} */
+/**
+ * コマンドのリストを表示する。コマンドをクリックするとその値を引数に与えてonClickを呼ぶ。
+ * @type {React.FC<{ onClick?: (value: string) => void }>}
+ */
+const CommandList = (props) => {
+  const list = /** @type {{ name: String, group: string[][] }[]} */([])
+  for (const fname of ['plugin_browser', 'plugin_turtle', 'plugin_system']) {
+    const groups = commandListJSON[fname]
+    if (!groups) {
+      console.log('command.jsonの[' + fname + ']が読み込めません。')
+      continue // 読み込みに失敗した場合
+    }
+    for (const name in groups) {
+      list.push({ name, group: groups[name] })
+    }
+  }
+
+  return <ul>{
+    list.map(({ group, name }) => <li key={name}>
+      <div style={{ color: '#55c' }}>{name}</div>
+      <div style={{ marginLeft: '12px' }}>{
+        group.map(([type, name, args], i) => {
+          const value = (type === '関数') ? ((args + '/').split('/')[0] + name + '。') : name
+          return <span key={i} style={{ marginRight: '12px', cursor: 'pointer' }} onClick={() => props.onClick?.(value)}>[{name}]</span>
+        })
+      }</div>
+    </li>)
+  }</ul>
+}
+
+/**
+ * エディタ本体
+ * @type {React.FC<{ code: string, editorId: number }>}
+ */
 const Editor = ({ code, editorId }) => {
   const preCode = `\
   # 自動実行されるコード (編集不可)
@@ -65,28 +99,41 @@ const Editor = ({ code, editorId }) => {
   }, [])
 
   return <div>
+    {/* プログラムの直前に自動挿入されるコードをreadonlyなエディタで表示する。 */}
     {React.useMemo(() => <div id={`nako3_editor_precode_${editorId}`} data-nako3-readonly style={{ height: '100px', borderBottom: 'gray 1px solid' }}>{preCode}</div>, [])}
+
+    {/* 実行したいプログラムを書くエディタ */}
     {React.useMemo(() => <div id={`nako3_editor_code_${editorId}`}>{code}</div>, [])}
+
+    {/* 実行ボタンなど */}
     <div className="buttons">
       <button className="default_button" onClick={run}>実行</button>
       <button className="default_button" onClick={test}>テスト</button>
       <button className="default_button" onClick={clear}>クリア</button>
       <button onClick={download}>↓</button>
     </div>
+
+    {/* 実行結果の表示用のdivと、ユーザープログラムから操作できるdivとキャンバス */}
     <div>
       <div className="edit_head">実行結果:</div>
       <div id={`nako3_editor_info_${editorId}`} className="info"></div>
       <div id={`nako3_div_${editorId}`}></div>
       <canvas id={`nako3_canvas_${editorId}`} width="310" height="150"/>
     </div>
+
+    {/* 使用した命令のリストを表示する。 */}
     <div>
       <div className="edit_head">使用した命令:</div>
       <p className="info">{Array.from(usedFuncs.values()).sort().join(', ')}</p>
     </div>
+
+    {/* テストの結果を表示する。 */}
     <div>
       <p className="edit_head">テスト結果:</p>
       <div id="mocha"/>
     </div>
+
+    {/* 命令の一覧を表示する。命令をクリックするとエディタのカーソル位置にその文字列が挿入される。 */}
     <div>
       <div style={{paddingTop: '8px'}}>
         <button onClick={toggleCommandList} className="default_button">
