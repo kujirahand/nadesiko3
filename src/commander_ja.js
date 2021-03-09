3232b90199da9ba2dc40214a9eb8796d88474256
yy0931 <54441600+yy0931@users.noreply.github.com>
2021-03-09 09:58:50 +0900

    commander_ja への型付け

--- src/commander_ja.js
+++ src/commander_ja.js
@@ -1,143 +1,155 @@
 // commander_ja.js
 const app = {
-    'args': [],
-    '_alias': {},
-    '_hasarg':{},
-    '_help': [],
-    '_usage': '',
-    '_version': '1.0.0',
-    '_title': ''
-}
-/**
- * set version info
- * @param {*} ver version info
- * @param {*} cmd -v, --version
- */
-app.version = (ver, cmd) => {
-    app.option(cmd, 'バージョン情報を表示')
-    app._version = ver
-    return app
-}
-app.title = (title) => {
-    app._title = title
-    return app
-}
-app.usage = (usage) => {
-    app._usage = usage
-    return app
-}
-app.option = (cmd, desc) => {
-    app._help.push([cmd, desc])
-    let name1 = ''
-    let name2 = []
-    const cmdList = cmd.split(',')
-    cmdList.forEach((t) => {
-        const c = t.replace(/\s+/, '')
-        const m = c.match(/^(\-+)([a-zA-Z0-9_]+)/)
-        if (!m) {
-            throw Error('[Invalid option]: should be -(shortname) --(longname): ' + cmd)
-        }
-        if (m[1] == '--') {
-            name1 = m[2]
-            return
-        }
-        if (m[1] == '-') {
-            name2.push(m[2])
-            return
-        }
-    })
-    if (name1 === '') {
-        throw Error('Invalid option(no longname): ' + cmd)
-    }
-    if (name1 === 'version') {name1='version_'}
-    app[name1] = false
-    name2.forEach((t) => {
-        app._alias[t] = name1
-    })
-    app._hasarg[name1] = (cmd.indexOf('[') >= 0)
-    return app
-}
-/**
- * parse and return str
- * @param {*} argv 
- * @return {string}
- */
-app.parseStr = (argv) => {
-    app['args'] = []
-    const paramStack = []
-    let lastOpt = ''
-    for (let i = 2; i < argv.length; i++) {
-        arg = argv[i]
-        // Not Options
-        if (arg.charAt(0) != '-') {
-            if (paramStack.length > 0) {
-              const argParam = paramStack.pop()
-              app[argParam] = arg
-              continue
+    args: /** @type {string[]} */([]),
+    _alias: /** @type {Record<string, string>} */({}),
+    _hasarg: /** @type {Record<string, boolean>} */({}),
+    _help: /** @type {[string, string][]} */([]),
+    _usage: '',
+    _version: '1.0.0',
+    _title: '',
+    /**
+     * set version info
+     * @param {*} ver version info
+     * @param {*} cmd -v, --version
+     */
+    version(ver, cmd) {
+        this.option(cmd, 'バージョン情報を表示')
+        this._version = ver
+        return this
+    },
+    /**
+     * @param {string} title
+     */
+    title(title) {
+        this._title = title
+        return this
+    },
+    /**
+     * @param {string} usage
+     */
+    usage(usage) {
+        this._usage = usage
+        return this
+    },
+    /**
+     * @param {string} cmd
+     * @param {string} desc
+     */
+    option(cmd, desc) {
+        app._help.push([cmd, desc])
+        let name1 = ''
+        let name2 = /** @type {string[]} */([])
+        const cmdList = cmd.split(',')
+        cmdList.forEach((t) => {
+            const c = t.replace(/\s+/, '')
+            const m = c.match(/^(\-+)([a-zA-Z0-9_]+)/)
+            if (!m) {
+                throw Error('[Invalid option]: should be -(shortname) --(longname): ' + cmd)
             }
-            app.args.push(arg)
-            continue
+            if (m[1] == '--') {
+                name1 = m[2]
+                return
+            }
+            if (m[1] == '-') {
+                name2.push(m[2])
+                return
+            }
+        })
+        if (name1 === '') {
+            throw Error('Invalid option(no longname): ' + cmd)
         }
-        // Options
-        if (arg.substr(0, 2) == '--') {
-            lastOpt = arg.substr(2)
-            if (lastOpt === 'version') {lastOpt='version_'}
-        } else {
-            // Short Option
-            const shortName = arg.substr(1)
-            if (app._alias[shortName]) {
-                lastOpt = app._alias[shortName]
-            } else {
-                // Not exists
+        if (name1 === 'version') {name1='version_'}
+        this[name1] = false
+        name2.forEach((t) => {
+            this._alias[t] = name1
+        })
+        this._hasarg[name1] = (cmd.indexOf('[') >= 0)
+        return this
+    },
+    /**
+     * parse and return str
+     * @param {string[]} argv 
+     * @return {string}
+     */
+    parseStr(argv) {
+        this['args'] = []
+        const paramStack = []
+        let lastOpt = ''
+        for (let i = 2; i < argv.length; i++) {
+            const arg = argv[i]
+            // Not Options
+            if (arg.charAt(0) != '-') {
+                if (paramStack.length > 0) {
+                const argParam = paramStack.pop()
+                this[argParam] = arg
+                continue
+                }
+                this.args.push(arg)
                 continue
             }
+            // Options
+            if (arg.substr(0, 2) == '--') {
+                lastOpt = arg.substr(2)
+                if (lastOpt === 'version') {lastOpt='version_'}
+            } else {
+                // Short Option
+                const shortName = arg.substr(1)
+                if (this._alias[shortName]) {
+                    lastOpt = this._alias[shortName]
+                } else {
+                    // Not exists
+                    continue
+                }
+            }
+            // set option true
+            this[lastOpt] = true
+            if (this._hasarg[lastOpt]) { // has argument?
+                // init parameter
+                this[lastOpt] = ''
+                paramStack.push(lastOpt)
+            }
         }
-        // set option true
-        app[lastOpt] = true
-        if (app._hasarg[lastOpt]) { // has argument?
-            // init parameter
-            app[lastOpt] = ''
-            paramStack.push(lastOpt)
+        // show version?
+        if (this.version_) {
+            return this._version
         }
-    }
-    // show version?
-    if (app.version_) {
-        return app._version
-    }
-    // show help?
-    if (app.help) {
-        return app.getHelp()
-    }
-    return ''
-}
-/**
- * parse
- * @param {*} argv 
- */
-app.parse = (argv) => {
-    const s = app.parseStr(argv)
-    if (s === '') {
-        return app
-    }
-    console.log(s)
-    process.exit(0)
-}
-app.option('-h, --help', 'コマンドの使い方を表示')
+        // show help?
+        if (this.help) {
+            return this.getHelp()
+        }
+        return ''
+    },
 
-app.getHelp = ()=> {
-    let ss = ''
-    if (app._title) {
-        ss += app._title + '\n'
+    /**
+     * parse
+     * @param {*} argv 
+     */
+    parse(argv) {
+        const s = this.parseStr(argv)
+        if (s === '') {
+            return this
+        }
+        console.log(s)
+        process.exit(0)
+    },
+
+    getHelp() {
+        let ss = ''
+        if (this._title) {
+            ss += this._title + '\n'
+        }
+        ss += '使い方: ' + this._usage + '\n'
+        ss += 'オプション:\n'
+        const spc = '                               '
+        this._help.forEach((c) =>{
+            const opt = c[0] + spc
+            ss += '  ' + opt.substr(0, 20) + ' ' + c[1] + '\n'
+        })
+        return ss
     }
-    ss += '使い方: ' + app._usage + '\n'
-    ss += 'オプション:\n'
-    const spc = '                               '
-    app._help.forEach((c) =>{
-        const opt = c[0] + spc
-        ss += '  ' + opt.substr(0, 20) + ' ' + c[1] + '\n'
-    })
-    return ss
 }
+
+app.option('-h, --help', 'コマンドの使い方を表示')
 
 // register app
 module.exports = app
