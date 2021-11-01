4d2542d11b781c1a1f3270797eb91c1398760209
kujirahand_colab <web@kujirahand.com>
2021-11-01 19:01:37 +0900

    ブラウザ版に「OS取得」命令を追加。#1089 extlibのダウンロードに失敗する問題を修正

--- src/plugin_browser_system.js
+++ src/plugin_browser_system.js
@@ -8,5 +8,20 @@ module.exports = {
       throw new Error('__終わる__')
     },
     return_none: true
+  },
+  'OS取得': { // @OSプラットフォームを返す(darwin/win32/linux/android/iphone/ipad/unknown) // @OSしゅとく
+    type: 'func',
+    josi: [],
+    pure: true,
+    fn: function () {
+      const ua = window.navigator.userAgent.toLowerCase()
+      if (ua.indexOf('windows') !== -1) { return "windows" }
+      if (ua.indexOf('android') !== -1) { return "android" }
+      if (ua.indexOf('iphone') !== -1) { return "iphone" }
+      if (ua.indexOf('ipad') !== -1) { return "ipad" }
+      if (ua.indexOf('mac os x') !== -1) { return "darwin" }
+      if (ua.indexOf('macintosh') !== -1) { return "darwin" }
+      return "unknown"
+    }
   }
 }
