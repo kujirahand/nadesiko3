1f495b80e76dd3dbab321a9048efe56e91f1a896
kujirahand_colab <web@kujirahand.com>
2022-02-07 17:30:52 +0900

    eslint-plugin-promise のバージョンの問題を修正

--- src/cnako3.js
+++ src/cnako3.js
@@ -3,14 +3,6 @@
 /**
  * コマンドライン版のなでしこ3
  */
-const fetch = require('node-fetch')
-if (typeof globalThis !== 'undefined' && typeof globalThis.fetch === 'undefined') {
-  globalThis.fetch = fetch
-} else
-if (typeof global !== 'undefined' && typeof global.fetch === 'undefined') {
-  global.fetch = fetch
-}
-
 const fs = require('fs')
 const exec = require('child_process').exec
 
