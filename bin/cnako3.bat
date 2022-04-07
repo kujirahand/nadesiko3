dddfdb18d3b81f8568370891a8462bef4e0d785a
kujirahand <web@kujirahand.com>
2022-04-07 20:24:52 +0900

    Windows版cnako3でnodeがグローバルにある場合も動くように修正。#1182

--- bin/cnako3.bat
+++ bin/cnako3.bat
@@ -6,6 +6,10 @@ popd
 
 SET SRC_DIR=%ROOT_DIR%\src
 SET CNAKO_JS=%SRC_DIR%\cnako3.js
-SET NODE_EXE=%ROOT_DIR%\nodejs\node.exe
+if exist %ROOT_DIR%\nodejs\node.exe (
+    SET NODE_EXE=%ROOT_DIR%\nodejs\node.exe
+) else (
+    SET NODE_EXE=node.exe
+)
 
 %NODE_EXE% %CNAKO_JS% %1 %2 %3 %4 %5 %6 %7 %8 %9
