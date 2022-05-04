d39b37234b6b6478f3d3738a6938f53a09e99da8
yy0931 <54441600+yy0931@users.noreply.github.com>
2022-05-04 18:56:50 +0900

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -2,7 +2,7 @@
 import {NakoCompiler} from './nako3.mjs'
 import {PluginNode} from './plugin_node.mjs'
 
-default export {
+export default {
   // Node.jsのモジュールモードでなでしこを利用する場合
   compiler: NakoCompiler,
   PluginNode
