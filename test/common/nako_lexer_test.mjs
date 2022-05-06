2704e1c6560574b6fcef4cd86908d315dd2fa1a1
kujirahand <web@kujirahand.com>
2022-05-06 23:17:00 +0900

    追加忘れのテストファイルを追加

---
+++ test/common/nako_lexer_test.mjs
@@ -0,0 +1,33 @@
+import assert from 'assert'
+import { NakoLexer } from '../../src/nako_lexer.mjs'
+import { NakoLogger } from '../../src/nako_logger.mjs'
+import { NakoPrepare } from '../../src/nako_prepare.mjs'
+
+describe('nako_lexer_test', () => {
+  const lex = new NakoLexer(new NakoLogger())
+  const pre = new NakoPrepare(lex.logger)
+  // --- test ---
+  it('トークンの区切りテスト', () => {
+    let a = lex.tokenize('Nは30', 0, 'test.nako3')
+    assert.strictEqual(NakoLexer.tokensToTypeStr(a, '|'), 'word|number')
+    let b = lex.tokenize('もしN=30ならば', 0, 'test.nako3')
+    assert.strictEqual(NakoLexer.tokensToTypeStr(b, '|'), 'もし|word|eq|number')
+  })
+  it('関数の登録テスト', () => {
+    const code = '●AAAとは\n「あ」を表示\nここまで。\n'
+    const code2 = pre.convert(code).map((v) => v.text).join('')
+    const tok = lex.tokenize(code2, 0, 'test.nako3')
+    const funclist = {}
+    NakoLexer.preDefineFunc(tok, lex.logger, funclist)
+    assert.strictEqual(funclist['test__AAA'].type, 'func')
+  })
+  it('変数は登録しないというテスト', () => {
+    const code = 'HOGE=333\n'
+    const code2 = pre.convert(code).map((v) => v.text).join('')
+    const tok = lex.tokenize(code2, 0, 'test.nako3')
+    const funclist = {}
+    NakoLexer.preDefineFunc(tok, lex.logger, funclist)
+    assert.strictEqual(funclist['HOGE'], undefined)
+  })
+})
+
