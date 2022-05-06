8f23f4240a43d5cb640e8e86401f8d56642d2fa3
kujirahand <web@kujirahand.com>
2022-05-06 15:31:28 +0900

    src/index.mjs で PluginNode のimportエラーが出る問題

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,6 @@
 
 import {NakoCompiler} from './nako3.mjs'
-import {PluginNode} from './plugin_node.mjs'
+import PluginNode from './plugin_node.mjs'
 
 export default {
   // Node.jsのモジュールモードでなでしこを利用する場合
