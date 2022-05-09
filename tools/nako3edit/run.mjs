3aab90f2087f060d2048c45f0909a06760f86843
kujirahand <web@kujirahand.com>
2022-05-10 00:02:13 +0900

    kaizen3@37 の通り、tools/nako3edit/run.mjs を修正

--- tools/nako3edit/run.mjs
+++ tools/nako3edit/run.mjs
@@ -9,7 +9,7 @@ const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const nakoHome = path.resolve(path.join(__dirname, '../../'))
-const cnako3 = path.resolve(path.join(nakoHome, 'src/cnako3.js'))
+const cnako3 = path.resolve(path.join(nakoHome, 'src/cnako3.mjs'))
 const nako3edit = path.resolve(path.join(__dirname, 'index.nako3'))
 
 let proc = spawn('node', [cnako3, nako3edit])
