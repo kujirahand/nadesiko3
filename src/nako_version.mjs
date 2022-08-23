12ee4f27f3bdfaba956edebb83e39c49a64ff399
kujirahand <web@kujirahand.com>
2022-08-24 00:28:45 +0900

    v3.3.62

--- src/nako_version.mjs
+++ src/nako_version.mjs
@@ -1,8 +1,8 @@
 // 実際のバージョン定義 (自動生成されるので以下を編集しない)
 const nakoVersion = {
-    version: '3.3.61',
+    version: '3.3.62',
     major: 3,
     minor: 3,
-    patch: 61
+    patch: 62
 };
 export default nakoVersion;
