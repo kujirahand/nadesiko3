f58e0990e7d47bb9ba16ff322c485c2ce32467ab
kujirahand <web@kujirahand.com>
2022-09-05 20:30:15 +0900

    インデント記号のタブ(\t)を有効に修正 #1333

--- src/nako_version.mjs
+++ src/nako_version.mjs
@@ -1,8 +1,8 @@
 // 実際のバージョン定義 (自動生成されるので以下を編集しない)
 const nakoVersion = {
-    version: '3.3.65',
+    version: '3.3.66',
     major: 3,
     minor: 3,
-    patch: 65
+    patch: 66
 };
 export default nakoVersion;
