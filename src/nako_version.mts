d0e5e355ddec0ea64bcb03af51d9d5ba4c5db56b
kujirahand <web@kujirahand.com>
2022-09-11 02:18:10 +0900

    文字列で指定したイベント関数も名前空間が解決できるように #1332

--- src/nako_version.mts
+++ src/nako_version.mts
@@ -11,9 +11,9 @@ export interface NakoVersion {
 }
 // 実際のバージョン定義 (自動生成されるので以下を編集しない)
 const nakoVersion: NakoVersion = {
-  version: '3.3.70',
+  version: '3.3.71',
   major: 3,
   minor: 3,
-  patch: 70
+  patch: 71
 }
 export default nakoVersion
--- test/node/scope_test.mjs
+++ test/node/scope_test.mjs
@@ -46,4 +46,14 @@ describe('scope_test', async () => {
     await cmp(`${codeInclude}\n●AAAとは\n3の三倍表示処理\nここまで。AAA;`, '9')
     await cmp(`${codeInclude}\n●AAAとは\n3の四倍表示処理\nここまで。AAA;`, '12')
   })
+  it('文字列で取り込み先の関数を呼び出す場合', async () => {
+    await cmp(`${codeInclude}\n[1,5,3,2,4]を「カスタムソート処理」で配列カスタムソートしてJSONエンコードして表示`, '[1,2,3,4,5]')
+    await cmp(`${codeInclude}\n[1,3,2]を「scope_c__カスタムソート処理」で配列カスタムソートしてJSONエンコードして表示`, '[1,2,3]')
+  })
+  it('取り込みつつ自分のスコープの関数を文字列で呼び出す場合', async () => {
+    await cmp(`${codeInclude}\n●(A,Bを)AAAとは\nA-Bを戻す\nここまで;[1,5,3,2,4]を「AAA」で配列カスタムソートしてJSONエンコードして表示`, '[1,2,3,4,5]')
+    await cmp(`${codeInclude}\n●(A,Bを)カスタムソート処理とは\nB-Aを戻す\nここまで;[1,3,2]を「カスタムソート処理」で配列カスタムソートしてJSONエンコードして表示`, '[3,2,1]')
+    await cmp(`${codeInclude}\n●(A,Bを)カスタムソート処理とは\nB-Aを戻す\nここまで;[1,3,2]を「scope_c__カスタムソート処理」で配列カスタムソートしてJSONエンコードして表示`, '[1,2,3]')
+    await cmp(`${codeInclude}\n●(A,Bを)カスタムソート処理とは\nB-Aを戻す\nここまで;[1,3,2]を「main__カスタムソート処理」で配列カスタムソートしてJSONエンコードして表示`, '[3,2,1]')
+  })
 })
