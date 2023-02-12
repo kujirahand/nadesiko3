cf0b1d1016f15f3358203c98ce553d104b610e4d
kujirahand <web@kujirahand.com>
2023-02-12 15:46:27 +0900

    「逐次実行」構文のテストがエラーになるので修正 #1164

--- test/node/async_test.mjs
+++ test/node/async_test.mjs
@@ -21,33 +21,30 @@ describe('async_test', async () => {
     const result = await nako3.runAsync(code)
     assert.strictEqual(result.log, exRes)
   }
-  const exe = (/** @type {string} */ code) => {
+  const exe = async (/** @type {string} */ code) => {
     const nako3 = new NakoCompiler()
     nako3.addPluginObject('PluginMyTest', PluginMyTest)
-    nako3.runSync(code)
+    await nako3.runAsync(code)
   }
 
   // assert test
   it('アサート自体のテスト', async () => {
-    exe('3と3でテスト。')
+    await exe('3と3でテスト。')
     await cmp('3を表示', '3')
   })
 
   // --- async ---
-  it('async_simple', (done) => {
-    global.done = done
-    exe(
+  it('async_simple', async () => {
+    await exe(
       '逐次実行\n' +
       '先に、1と表示\n' +
       '次に、2と表示\n' +
       '次に、表示ログと「1\n2\n」でテスト。\n' +
-      '次に、「global.done()」をJS実行。' +
       'ここまで。\n'
     )
   })
-  it('async_multiple', (done) => {
-    global.done = done
-    exe(
+  it('async_multiple', async () => {
+    await exe(
       '逐次実行\n' +
       '先に\n' +
       '  1と表示\n' +
@@ -55,39 +52,32 @@ describe('async_test', async () => {
       'ここまで\n' +
       '次に、3と表示\n' +
       '次に、表示ログと「1\n2\n3\n」でテスト。\n' +
-      '次に、「global.done()」をJS実行。' +
       'ここまで。\n'
     )
   })
-  it('戻り値を使う', (done) => {
-    global.done = done
-    exe(
+  it('戻り値を使う', async () => {
+    await exe(
       '逐次実行\n' +
       '先に、それは30\n' +
       '次に、それを表示\n' +
       '次に、表示ログと「30\n」でテスト。\n' +
-      '次に、「global.done()」をJS実行。' +
       'ここまで。\n'
     )
   })
-  it('同期タイマー', (done) => {
-    global.done = done
-    exe(
+  it('同期タイマー', async () => {
+    await exe(
       '逐次実行\n' +
       '先に、0.01秒待機\n' +
       '次に、30を表示\n' +
       '次に、表示ログと「30\n」でテスト。\n' +
-      '次に、「global.done()」をJS実行。\n' +
       'ここまで。\n'
     )
   })
-  it('連文 #373', (done) => {
-    global.done = done
-    exe(
+  it('連文 #373', async () => {
+    await exe(
       '逐次実行\n' +
       '先に、30に5を足して表示\n' +
       '次に、表示ログと「35\n」でテスト。\n' +
-      '次に、「global.done()」をJS実行。\n' +
       'ここまで。\n'
     )
   })
