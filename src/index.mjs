a06dee41fcacb9d1c8ea90bc453b9fa8d343357a
kujirahand <web@kujirahand.com>
2023-12-30 09:00:12 +0900

    mtsファイルから生成されるmjsファイルをリポジトリから取り除く #1608

--- src/index.mjs
+++
@@ -1,9 +0,0 @@
-import { NakoCompiler } from '../core/src/nako3.mjs';
-import { CNako3 } from './cnako3mod.mjs';
-import PluginNode from './plugin_node.mjs';
-export default {
-    CNako3,
-    NakoCompiler,
-    compiler: NakoCompiler,
-    PluginNode
-};
--- src/nako_version.mjs
+++
@@ -1,8 +0,0 @@
-// 実際のバージョン定義 (自動生成されるので以下を編集しない)
-const nakoVersion = {
-    version: '3.4.24',
-    major: 3,
-    minor: 4,
-    patch: 24
-};
-export default nakoVersion;
