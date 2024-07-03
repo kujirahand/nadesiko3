fe8ef0f12a44203bbbb2a67c4637ccc675476703
kujirahand <web@kujirahand.com>
2024-07-03 11:46:25 +0900

    esbuildでplugin_weykturtle3d.mtsに変更 #1690

--- bundle_esbuild.ts
+++ bundle_esbuild.ts
@@ -7,13 +7,14 @@ const files = [
   // mjs
   'src/wnako3webworker.mjs',
   'src/plugin_webworker.mjs',
-  'src/plugin_weykturtle3d.mjs',
   'src/plugin_kansuji.mjs',
   'src/plugin_markup.mjs',
   'src/plugin_datetime.mjs',
   'src/plugin_caniuse.mjs',
   // mts
+  'src/plugin_three.mts',
   'src/plugin_turtle.mts',
+  'src/plugin_weykturtle3d.mts',
   'editor/edit_main.jsx',
   'editor/version_main.jsx',
 ]
